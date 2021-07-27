0f6374a88f1ae2d10bcbd8824472322ea139ffb5
Nic <nicollecanuton@gmail.com>
2021-07-27 10:56:53 -0300

    class person

---
+++ RestWithASPNET/RestWithASPNET/Model/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET.Model
+{
+    public class Person
+    {
+        public long id { get; set; }
+        public string fname { get; set; }
+        public string lname { get; set; }
+        public string adress { get; set; }
+        public string gender { get; set; }
+    }
+}
