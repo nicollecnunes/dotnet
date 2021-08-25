f666e0ce553d52024c0d6be54246445b5bdbad44
Nic <nicollecanuton@gmail.com>
2021-08-25 14:23:10 -0300

    testing serialization

--- RestWithASPNET/RestWithASPNET/Data/VO/PersonVO.cs
+++ RestWithASPNET/RestWithASPNET/Data/VO/PersonVO.cs
@@ -1,6 +1,10 @@
-﻿namespace RestWithASPNET.Data.VO{ //value object
+﻿//using System.Text.Json.Serialization;
+
+namespace RestWithASPNET.Data.VO{ //value object
 
     public class PersonVO{
+        //[JsonPropertyName("FrontNameIdHere")]
+        //[JsonIgnore] não serializa
         public long id {get; set;}
         public string address { get; set; }
         public string fname { get; set; }
